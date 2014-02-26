0eaff0a8e60c8bc42f1a51655164d107c9e95ea6
FelipeBarreto <felipemota0301@gmail.com>
2014-02-26 15:47:38 -0300

    Criado um Toast indicando que a aplicação foi criada com sucesso.

--- GitText2/src/com/example/gittext2/MainActivity.java
+++ GitText2/src/com/example/gittext2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gittext2;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Toast.makeText(this, "Aplicação criada", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
